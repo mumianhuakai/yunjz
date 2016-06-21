b5cd492ba362eea976e5668a8d2d9725a4f1c9c1
mumu <mumu_lin00@126.com>
2016-06-21 10:22:11 +0800

    update code

--- yunjz_prj/accounts/forms.py
+++ yunjz_prj/accounts/forms.py
@@ -4,17 +4,22 @@ from django.contrib.auth.models import User
 
 #Register your models here
 ALLOW_CHAR="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+#定义注册表单，这个表单继承自forms.form,这是django自带的，首先写出该类中
+#涉及的字段
 class RegisterForm(forms.Form):
     username=forms.CharField(label=u"昵称",max_length=40,
+	#定义插件，用什么来展示，Emailnput自动对email的格式进行验证
 	widget=forms.TextInput(attrs={'size':40,'class':"form-control"}))
     email=forms.EmailField(label=u"邮件",max_length=40,
+	#Emailnput自动对email的格式进行验证
 	widget=forms.EmailInput(attrs={'size':40,'class':"form-control"}))
     password=forms.CharField(label=u"密码",max_length=20,
+	#PasswordInput自动把你输入的密码转换为*显示
 	widget=forms.PasswordInput(attrs={'size':20,'class':"form-control"}))
     re_password=forms.CharField(label=u"重复密码",max_length=20,
 	widget=forms.PasswordInput(attrs={'size':20,'class':"form-control"}))
     def clean_username(self):
-        #验证昵称
+        #验证昵称,用self.cleaned_data["username"]获取用户提交的数据
 	if len(self.cleaned_data["username"])<4:
 	    raise forms.ValidationError(u"昵称长度不能小于4")
         else:
@@ -40,8 +45,9 @@ class RegisterForm(forms.Form):
 	        if a not in ALLOW_CHAR:
 		    raise forms.ValidationError(u"密码仅能用字母或数字")
         return self.cleaned_data["password"]
+    #多个字段相互影响的话，就需要这个clean
     def clean(self):
-        #验证其他非法
+        #验证其他非法,采用super（RegisterForm，self）.clean()获取用户提交的数据
         cleaned_data = super(RegisterForm, self).clean()
         if cleaned_data.get("password") == cleaned_data.get("username"):
 	    raise forms.ValidationError(u"用户名与密码不能一样")
--- yunjz_prj/accounts/views.py
+++ yunjz_prj/accounts/views.py
@@ -18,23 +18,29 @@ def register(request):
 	template_var={}
 	form=RegisterForm()
 	if request.method == "POST":
+	    #使用表单时直接request.POST.copy()这样把POST数据copy一份给表单
 	    form=RegisterForm(request.POST.copy())
+	    #生成表单之后，调用is_valid检查它的正确性，把clean那些数据全部执行
+	    #一遍，把error信息全部放在form里,放进区之后怎么表现出来？
 	    if form.is_valid():
 	        username=form.cleaned_data["username"]
 	        email=form.cleaned_data["email"]
-	        password=form.cleaned_data["password"]
+		password=form.cleaned_data["password"]
 		#获得用户名、密码、email后用这三者为参数创建用户
 	        user=User.objects.create_user(username,email,password)
 		#创建好用户之后要保存倒数据库中
 	        user.save()
 	        if _login(request,username,password,template_var):
 	            return HttpResponseRedirect("/accounts/index")
+	#把form中的错误信息填到模板对应得变量中,就可以在模板templates/accounts/
+	#对应的html文件中展示出错误信息
 	template_var["form"]=form
 	return render(request,"accounts/register.html",template_var)
 
 def login(request):
 	template_var={}
 	if request.method == "POST":
+	    #获取用户填入的数据，获取数据后就可以对它进行判断
 	    username = request.POST.get("username")
 	    password=request.POST.get("password")
 	    if _login(request,username,password,template_var):
